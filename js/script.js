596ac0b2517d58933d8e12f56a124ac950b1e285
Rhudsonl <105531444+Rhudsonl@users.noreply.github.com>
2022-12-03 01:32:29 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+import { IMC, notNumber } from "./utils.js"
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
+
+    if (weightOrHeightIsNotANumber) {
+        AlertError.open()
+        return 
+    } 
+
+    AlertError.close()
+
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+    
+    Modal.message.innerText = message
+    Modal.open()
+
+}
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
